5ca58d5cde1cedd683c305abdec98b61a0542cf4
Kerosene21 <136657340+Kerosene21@users.noreply.github.com>
2024-10-21 19:04:50 -0500

    Update regresion_lineal.h

--- ProyectoFinal/C++/regresion_lineal.h
+++ ProyectoFinal/C++/regresion_lineal.h
@@ -1,1 +1,19 @@
+#ifndef REGRESION_LINEAL_H
+#define REGRESION_LINEAL_H
+
+#include <vector>
+
+class RegresionLineal {
+public:
+    RegresionLineal();
+    void entrenar(const std::vector<double>& x, const std::vector<double>& y);
+    double predecir(double valor);
+    double calcularMSE(const std::vector<double>& x, const std::vector<double>& y);
+
+private:
+    double pendiente;
+    double intercepto;
+};
+
+#endif
 
