5c6998a7a96f5cd2db2ee286350a59e5b9980e61
Kerosene21 <136657340+Kerosene21@users.noreply.github.com>
2024-10-21 19:31:10 -0500

    Update Producto.java

--- ProyectoFinal/Java/src/models/Producto.java
+++ ProyectoFinal/Java/src/models/Producto.java
@@ -1,1 +1,32 @@
+package models;
+
+import java.io.Serializable;
+
+public class Producto implements Serializable {
+    private String nombre;
+    private double precioBase;
+    private int cantidadDisponible;
+
+    public Producto(String nombre, double precioBase, int cantidadDisponible) {
+        this.nombre = nombre;
+        this.precioBase = precioBase;
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
+    // Getters y Setters
+    public String getNombre() { return nombre; }
+    public double getPrecioBase() { return precioBase; }
+    public int getCantidadDisponible() { return cantidadDisponible; }
+    
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setPrecioBase(double precioBase) { this.precioBase = precioBase; }
+    public void setCantidadDisponible(int cantidadDisponible) { 
+        this.cantidadDisponible = cantidadDisponible; 
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " - Precio: " + precioBase + " - Cantidad: " + cantidadDisponible;
+    }
+}
 
