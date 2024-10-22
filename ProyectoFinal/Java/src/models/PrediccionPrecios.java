5aabd7e9743cc00faeba4345d0b9ff010535f634
Kerosene21 <136657340+Kerosene21@users.noreply.github.com>
2024-10-21 19:26:40 -0500

    Update PrediccionPrecios.java

--- ProyectoFinal/Java/src/models/PrediccionPrecios.java
+++ ProyectoFinal/Java/src/models/PrediccionPrecios.java
@@ -1,1 +1,18 @@
+package models;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PrediccionPrecios {
+    public static double predecir(double valor) {
+        try {
+            Process process = new ProcessBuilder("./prediccion_cpp", String.valueOf(valor)).start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            return Double.parseDouble(reader.readLine());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0.0;
+        }
+    }
+}
 
