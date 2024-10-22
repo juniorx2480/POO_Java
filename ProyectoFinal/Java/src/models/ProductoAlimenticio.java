0ec2df2bb1ab2bfb26f4f48c7282602291376c01
Kerosene21 <136657340+Kerosene21@users.noreply.github.com>
2024-10-21 19:37:39 -0500

    Update ProductoAlimenticio.java

--- ProyectoFinal/Java/src/models/ProductoAlimenticio.java
+++ ProyectoFinal/Java/src/models/ProductoAlimenticio.java
@@ -1,1 +1,24 @@
+package models;
+
+import java.time.LocalDate;
+
+public class ProductoAlimenticio extends Producto {
+    private LocalDate fechaExpiracion;
+
+    public ProductoAlimenticio(String nombre, double precioBase, int cantidadDisponible, LocalDate fechaExpiracion) {
+        super(nombre, precioBase, cantidadDisponible);
+        this.fechaExpiracion = fechaExpiracion;
+    }
+
+    // Getter y Setter para fechaExpiracion
+    public LocalDate getFechaExpiracion() { return fechaExpiracion; }
+    public void setFechaExpiracion(LocalDate fechaExpiracion) { 
+        this.fechaExpiracion = fechaExpiracion; 
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " - Fecha de Expiración: " + fechaExpiracion;
+    }
+}
 
