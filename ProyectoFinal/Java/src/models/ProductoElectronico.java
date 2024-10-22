c7728e64ad8a1e4ce640414159b428b6532257e1
Kerosene21 <136657340+Kerosene21@users.noreply.github.com>
2024-10-21 19:37:15 -0500

    Update ProductoElectronico.java

--- ProyectoFinal/Java/src/models/ProductoElectronico.java
+++ ProyectoFinal/Java/src/models/ProductoElectronico.java
@@ -1,1 +1,20 @@
+package models;
+
+public class ProductoElectronico extends Producto {
+    private int garantiaMeses;
+
+    public ProductoElectronico(String nombre, double precioBase, int cantidadDisponible, int garantiaMeses) {
+        super(nombre, precioBase, cantidadDisponible);
+        this.garantiaMeses = garantiaMeses;
+    }
+
+    // Getter y Setter para garantiaMeses
+    public int getGarantiaMeses() { return garantiaMeses; }
+    public void setGarantiaMeses(int garantiaMeses) { this.garantiaMeses = garantiaMeses; }
+
+    @Override
+    public String toString() {
+        return super.toString() + " - Garantía: " + garantiaMeses + " meses";
+    }
+}
 
