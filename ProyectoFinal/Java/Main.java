7860308b64a927c6e6a2114a832156e399d44aac
Kerosene21 <136657340+Kerosene21@users.noreply.github.com>
2024-10-21 19:41:46 -0500

    Update Main.java

--- ProyectoFinal/Java/Main.java
+++ ProyectoFinal/Java/Main.java
@@ -1,1 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Cargar la vista desde el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/VentasView.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        // Configurar la ventana principal
+        primaryStage.setTitle("Sistema de Gestión de Productos");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        // Iniciar la aplicación JavaFX
+        launch(args);
+    }
+}
 
